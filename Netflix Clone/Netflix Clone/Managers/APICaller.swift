921f4b27065b195ffb649c3432c8598d5542f594
Mekala Vamsi Krishna <vamsikrishnayadav2002@gmail.com>
2022-10-24 21:59:15 +0530

    1. Querying Database for individual movie, 2. Using Youtube API, 3. Parsing Youtube API, 4. Handling Selections of cells, 5. Creating TitlePreviewController, 6. Refactoring TableViewHeader Hero Title and 7 Handling Tapping across all ViewControllers.

--- Netflix Clone/Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Netflix Clone/Managers/APICaller.swift
@@ -10,6 +10,8 @@ import Foundation
 struct Constants {
     static let API_KEY = "34f1a8f9c59360b0ca5e6cddb936a2d8"
     static let baseURL = "https://api.themoviedb.org"
+    static let YoutubeAPI_KEY = "AIzaSyA6fb4tKahOZ22wnHznWNDkbQTrjSUYHBM"
+    static let YoutubeBaseURL = "https://youtube.googleapis.com/youtube/v3/search?"
 }
 
 enum APIError: Error {
@@ -133,6 +135,23 @@ class APICaller {
             do {
                 let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
                 completion(.success(results.results))
+            } catch {
+                completion(.failure(APIError.failedToGetData))
+            }
+        }
+        task.resume()
+    }
+    
+    func getMovie(with query: String, completion: @escaping (Result<VideoElement, Error>) -> Void) {
+        guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else { return }
+        guard let url = URL(string: "\(Constants.YoutubeBaseURL)q=\(query)&key=\(Constants.YoutubeAPI_KEY)") else { return }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else { return }
+            
+            do {
+                let results = try JSONDecoder().decode(YoutubeSearchResponse.self, from: data)
+                completion(.success(results.items[0]))
             } catch {
                 completion(.failure(APIError.failedToGetData))
             }
